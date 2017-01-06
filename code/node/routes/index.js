7cf172489ab598aa4b05ff578fbd0c79a56ea1c8
inlost <sailflyer@gmail.com>
2017-01-06 16:24:57 +0800

    add blog demo

--- code/node/routes/index.js
+++ code/node/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var src = path.join(process.cwd(), 'md');
+    var finalList = [];
+
+    fs.readdir(src, function(err, files){
+        if(err){
+            return res.send('服务器忙');
+        }
+        console.log(files);
+        files.forEach((item, index, list) => {
+            var tmpArr = item.split('.');
+            var strDocType = `.${tmpArr[tmpArr.length - 1]}`;
+
+            if(strDocType.toLowerCase() === '.md'){
+                finalList.push(item.replace(strDocType, ''))
+            }
+        });
+        res.send({list: finalList});
+    })
 });
 
 module.exports = router;
---
+++ code/node/routes/posts.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var md = require('node-markdown').Markdown;
+var fs = require('fs');
+var path = require('path');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    var strHtml = '';
+    var strFile = '';
+    var filename = req.query.p;
+    var src = path.join(process.cwd(), 'md', `${filename}.md`);
+
+    fs.readFile(src, 'utf-8', function(err, data){
+        if(err){
+            return res.send('服务器开小差了.');
+        }
+        strHtml = md(data);
+        res.send(strHtml);
+    });
+});
+
+module.exports = router;
