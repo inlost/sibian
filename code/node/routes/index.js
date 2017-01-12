41c6531f55093cf7226ecf2b09d83a577a540f46
inlost <sailflyer@gmail.com>
2017-01-12 12:06:05 +0800

    update list api

--- code/node/routes/index.js
+++ code/node/routes/index.js
@@ -5,23 +5,51 @@ var path = require('path');
 var conf = require('../config');
 
 /* GET home page. */
+/**
+ * 获取文章列表
+ * @url       /
+ * @method    GET
+ * @return
+ *  success{boolean} 是否保存成功 true|false
+ *  errorMessage{string} 错误消息
+ *  data{array} 文章列表
+ *      name 文件名
+ *      create 创建时间
+ *      lastModify 最后更新时间
+ */
+
+
 router.get('/', function(req, res, next) {
     var src = path.join(process.cwd(), conf.markdownDir);
     var finalList = [];
 
     fs.readdir(src, function(err, files){
         if(err){
-            return res.send('服务器忙');
+            return res.json({
+                success: false,
+                errorMessage: '服务器忙'
+            });
         }
         files.forEach((item, index, list) => {
             var tmpArr = item.split('.');
             var strDocType = `.${tmpArr[tmpArr.length - 1]}`;
+            var mdSrc = '';
 
             if(strDocType.toLowerCase() === '.md'){
-                finalList.push(item.replace(strDocType, ''))
+                mdSrc = path.join(src, item);
+                var sta = fs.statSync(mdSrc);
+                finalList.push({
+                    name: item.replace(strDocType, ''),
+                    create: sta.ctime.toLocaleDateString(),
+                    lastModify: sta.ctime.toLocaleDateString()
+                });
             }
         });
-        res.send({list: finalList});
+        res.json({
+            success: true,
+            errorMessage: '',
+            data: finalList
+        });
     })
 });
 
